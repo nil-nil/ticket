b4bf0561c59641994f1be8d8f93c7afc7a84b508
tom.com <78278478+nil-nil@users.noreply.github.com>
2023-09-01 14:34:33 +0100

    alias service

--- internal/domain/alias.go
+++ internal/domain/alias.go
@@ -2,6 +2,16 @@ package domain
 
 import "fmt"
 
+type AliasRepository interface {
+	Find(FindAliasParameters) (Alias, error)
+}
+
+type FindAliasParameters struct {
+	ID     *uint64
+	Domain *string
+	User   *string
+}
+
 type Alias struct {
 	ID     uint64
 	User   string
@@ -10,4 +20,18 @@ type Alias struct {
 
 func (a *Alias) GetEmail() string {
 	return fmt.Sprintf("%s@%s", a.User, a.Domain)
+}
+
+func NewAliasService(repo AliasRepository) *AliasService {
+	return &AliasService{
+		repo: repo,
+	}
+}
+
+type AliasService struct {
+	repo AliasRepository
+}
+
+func (s *AliasService) Find(params FindAliasParameters) (Alias, error) {
+	return s.repo.Find(params)
 }
--- internal/domain/alias_test.go
+++ internal/domain/alias_test.go
@@ -1,10 +1,12 @@
 package domain_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/nil-nil/ticket/internal/domain"
 	"github.com/stretchr/testify/assert"
+	"k8s.io/utils/ptr"
 )
 
 func TestAliasEmail(t *testing.T) {
@@ -24,4 +26,41 @@ func TestAliasEmail(t *testing.T) {
 			assert.Equal(t, tc.expect, email)
 		})
 	}
+}
+
+type mockAliasRepo struct {
+	aliases map[string]domain.Alias
+}
+
+func (m *mockAliasRepo) Find(params domain.FindAliasParameters) (domain.Alias, error) {
+	if params.User == nil || params.Domain == nil {
+		return domain.Alias{}, domain.ErrNotFound
+	}
+	email := fmt.Sprintf("%s@%s", *params.User, *params.Domain)
+
+	alias, ok := m.aliases[email]
+	if !ok {
+		return domain.Alias{}, domain.ErrNotFound
+	}
+
+	return alias, nil
+}
+
+func TestGetAlias(t *testing.T) {
+	var repo = mockAliasRepo{
+		aliases: map[string]domain.Alias{
+			"test@test.com":      {ID: 1, User: "test", Domain: "test.com"},
+			"sample@example.com": {ID: 1, User: "sample", Domain: "example.com"},
+		},
+	}
+
+	svc := domain.NewAliasService(&repo)
+
+	alias, err := svc.Find(domain.FindAliasParameters{User: ptr.To("bob"), Domain: ptr.To("sample.com")})
+	assert.Equal(t, domain.Alias{}, alias, "alias should be empty")
+	assert.EqualError(t, domain.ErrNotFound, err.Error(), "expected not found error")
+
+	alias, err = svc.Find(domain.FindAliasParameters{User: ptr.To("test"), Domain: ptr.To("test.com")})
+	assert.Equal(t, domain.Alias{ID: 1, User: "test", Domain: "test.com"}, alias, "alias should not be empty")
+	assert.NoError(t, err, "error should be nil")
 }
