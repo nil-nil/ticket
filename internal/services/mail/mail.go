d3d7d59b9a0413a5b26f7b1b7b8b8cf3206d48af
tom.com <78278478+nil-nil@users.noreply.github.com>
2023-09-01 15:35:36 +0100

    Refactor mail server

--- internal/services/mail/mail.go
+++ internal/services/mail/mail.go
@@ -2,7 +2,6 @@ package mail
 
 import (
 	"errors"
-	"strings"
 
 	"github.com/nil-nil/ticket/internal/domain"
 )
@@ -12,28 +11,42 @@ var (
 	ErrInvalidEmailAddress = errors.New("invalid email address")
 )
 
-type server struct {
-	AuthFunc       AuthFunc
-	blockedSenders map[string]struct{}
+type MailServerRepository interface {
+	IsAuthoritative(domain string) bool
+	IsBlocked(address string) bool
 }
 
 type AuthFunc func(username, password string) (domain.User, error)
 
-func (s *server) ValidateSenderAddress(address string) error {
-	if _, exists := s.blockedSenders[address]; exists {
+func NewServer(mailServerRepo MailServerRepository, aliasRepo domain.AliasRepository, authFunc AuthFunc) *Server {
+	return &Server{
+		AuthFunc:   authFunc,
+		repository: mailServerRepo,
+		aliases:    domain.NewAliasService(aliasRepo),
+	}
+}
+
+type Server struct {
+	AuthFunc   AuthFunc
+	repository MailServerRepository
+	aliases    *domain.AliasService
+}
+
+func (s *Server) ValidateSenderAddress(address string) error {
+	if s.repository.IsBlocked(address) {
 		return ErrBlockedSender
 	}
 	return nil
 }
 
-func (s *server) ValidateRecipientAddress(address string) error {
+func (s *Server) ValidateRecipientAddress(address string) error {
 	return nil
 }
 
-func getUserAndDomainParts(address string) (user, domain string, err error) {
-	parts := strings.Split(address, "@")
-	if len(parts) != 2 || parts[0] == "" || parts[1] == "" {
-		return user, domain, ErrInvalidEmailAddress
-	}
-	return parts[0], parts[1], nil
-}
+// func getUserAndDomainParts(address string) (user, domain string, err error) {
+// 	parts := strings.Split(address, "@")
+// 	if len(parts) != 2 || parts[0] == "" || parts[1] == "" {
+// 		return user, domain, ErrInvalidEmailAddress
+// 	}
+// 	return parts[0], parts[1], nil
+// }
--- internal/services/mail/mail_test.go
+++ internal/services/mail/mail_test.go
@@ -1,15 +1,33 @@
-package mail
+package mail_test
 
 import (
 	"testing"
 
+	"github.com/nil-nil/ticket/internal/services/mail"
 	"github.com/stretchr/testify/assert"
 )
 
+type mockMailServerRepository struct {
+	authoritativeDomains map[string]struct{}
+	blockedSenders       map[string]struct{}
+}
+
+func (m *mockMailServerRepository) IsAuthoritative(domain string) bool {
+	_, ok := m.authoritativeDomains[domain]
+	return ok
+}
+
+func (m *mockMailServerRepository) IsBlocked(address string) bool {
+	_, ok := m.blockedSenders[address]
+	return ok
+}
+
 func TestValidateSenderAddress(t *testing.T) {
-	server := server{
+	repo := &mockMailServerRepository{
 		blockedSenders: map[string]struct{}{"bob@example.com": {}},
 	}
+	server := mail.NewServer(repo, nil, nil)
+
 	t.Run("test valid sender", func(t *testing.T) {
 		err := server.ValidateSenderAddress("alan@example.com")
 		assert.NoError(t, err)
@@ -17,40 +35,44 @@ func TestValidateSenderAddress(t *testing.T) {
 
 	t.Run("test blocked sender", func(t *testing.T) {
 		err := server.ValidateSenderAddress("bob@example.com")
-		assert.EqualError(t, err, ErrBlockedSender.Error())
+		assert.EqualError(t, err, mail.ErrBlockedSender.Error())
 	})
 }
 
 func TestValidateRecipientAddress(t *testing.T) {
-	server := server{}
+	repo := &mockMailServerRepository{
+		blockedSenders: map[string]struct{}{"bob@example.com": {}},
+	}
+	server := mail.NewServer(repo, nil, nil)
+
 	t.Run("test valid recipient", func(t *testing.T) {
 		err := server.ValidateRecipientAddress("alan@example.com")
 		assert.NoError(t, err)
 	})
 }
 
-func TestGetUserAndDomainParts(t *testing.T) {
-	cases := []struct {
-		description  string
-		address      string
-		expectUser   string
-		expectDomain string
-		expectErr    error
-	}{
-		{description: "valid email", address: "test@test.com", expectUser: "test", expectDomain: "test.com", expectErr: nil},
-		{description: "missing domain", address: "bob@", expectUser: "", expectDomain: "", expectErr: ErrInvalidEmailAddress},
-		{description: "missing user", address: "@nob", expectUser: "", expectDomain: "", expectErr: ErrInvalidEmailAddress},
-		{description: "not even an @ sign", address: "gary", expectUser: "", expectDomain: "", expectErr: ErrInvalidEmailAddress},
-	}
+// func TestGetUserAndDomainParts(t *testing.T) {
+// 	cases := []struct {
+// 		description  string
+// 		address      string
+// 		expectUser   string
+// 		expectDomain string
+// 		expectErr    error
+// 	}{
+// 		{description: "valid email", address: "test@test.com", expectUser: "test", expectDomain: "test.com", expectErr: nil},
+// 		{description: "missing domain", address: "bob@", expectUser: "", expectDomain: "", expectErr: ErrInvalidEmailAddress},
+// 		{description: "missing user", address: "@nob", expectUser: "", expectDomain: "", expectErr: ErrInvalidEmailAddress},
+// 		{description: "not even an @ sign", address: "gary", expectUser: "", expectDomain: "", expectErr: ErrInvalidEmailAddress},
+// 	}
 
-	for _, testCase := range cases {
-		t.Run(testCase.description, func(t *testing.T) {
-			user, domain, err := getUserAndDomainParts(testCase.address)
-			assert.Equal(t, testCase.expectUser, user, "user doesn't match")
-			assert.Equal(t, testCase.expectDomain, domain, "domain doesn't match")
-			if testCase.expectErr != nil {
-				assert.EqualError(t, err, testCase.expectErr.Error(), "err doesn't match")
-			}
-		})
-	}
-}
+// 	for _, testCase := range cases {
+// 		t.Run(testCase.description, func(t *testing.T) {
+// 			user, domain, err := getUserAndDomainParts(testCase.address)
+// 			assert.Equal(t, testCase.expectUser, user, "user doesn't match")
+// 			assert.Equal(t, testCase.expectDomain, domain, "domain doesn't match")
+// 			if testCase.expectErr != nil {
+// 				assert.EqualError(t, err, testCase.expectErr.Error(), "err doesn't match")
+// 			}
+// 		})
+// 	}
+// }
