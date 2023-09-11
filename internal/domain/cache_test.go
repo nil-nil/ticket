24732b4751c19caf3f11f8b21c77bbefc7f3ea20
tom.com <78278478+nil-nil@users.noreply.github.com>
2023-09-11 11:10:38 +0100

    More cache tests

--- internal/domain/cache_test.go
+++ internal/domain/cache_test.go
@@ -28,4 +28,36 @@ func TestCache(t *testing.T) {
 		err = c.Forget("")
 		assert.EqualError(t, err, domain.ErrCacheKeyInvalid.Error(), "Cache.Forget(\"\") should return ErrInvalidCacheKey")
 	})
+
+	t.Run("TestSettingCache", func(t *testing.T) {
+		err := c.Set("1", "examplestring")
+		assert.NoError(t, err, "valid Cache.Set() should not error")
+		assert.Equal(t, "examplestring", mockCache.cache["test.1"])
+	})
+
+	t.Run("TestGettingValidCache", func(t *testing.T) {
+		val, err := c.Get("1")
+		assert.NoError(t, err, "valid Cache.Get() should not error")
+		assert.Equal(t, "examplestring", val)
+	})
+
+	t.Run("TestGettingInvalidTypedCache", func(t *testing.T) {
+		mockCache.cache["test.2"] = 100
+		val, err := c.Get("2")
+		assert.EqualError(t, err, domain.ErrCachedValueInvalid.Error(), "invalid Cache.Get() should return meaningful error")
+		assert.Zero(t, val, "errored Cache.Get() should return zero value")
+	})
+
+	t.Run("TestGettingEmptyCache", func(t *testing.T) {
+		val, err := c.Get("3")
+		assert.EqualError(t, err, domain.ErrNotFoundInCache.Error(), "empty Cache.Get() should return meaningful error")
+		assert.Zero(t, val, "errored Cache.Get() should return zero value")
+	})
+
+	t.Run("TestForgettingCache", func(t *testing.T) {
+		err := c.Forget("1")
+		assert.NoError(t, err, "valid Cache.Forget() should not error")
+		_, ok := mockCache.cache["test.1"]
+		assert.False(t, ok, "forgotten cache item should be removed")
+	})
 }
