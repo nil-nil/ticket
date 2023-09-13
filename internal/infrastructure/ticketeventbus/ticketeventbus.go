217faf36d80c7431e4bab8aaf2aef1ac9e902aa7
tom.com <78278478+nil-nil@users.noreply.github.com>
2023-09-13 03:49:52 +0100

    Matcher

--- internal/infrastructure/ticketeventbus/ticketeventbus.go
+++ internal/infrastructure/ticketeventbus/ticketeventbus.go
@@ -40,11 +40,68 @@ func (t *ticketEventBus) sub(topic string, f func(eventKey string, data interfac
 	return nil
 }
 
-func (t *ticketEventBus) match(topic string) []func(eventKey string, data interface{}) {
-	return nil
+func (t *ticketEventBus) match(topic string) (subs []func(eventKey string, data interface{})) {
+	parts := strings.Split(topic, t.separator)
+	if len(parts) != 3 {
+		return nil
+	}
+
+	if _, ok := t.subs[parts[0]]; ok {
+		l1 := parts[0]
+		if _, ok := t.subs[l1][parts[1]]; ok {
+			l2 := parts[1]
+			if _, ok := t.subs[l1][l2][parts[2]]; ok {
+				subs = append(subs, t.subs[l1][l2][parts[2]]...)
+			}
+			if _, ok := t.subs[l1][l2]["*"]; ok && parts[2] != "*" {
+				subs = append(subs, t.subs[l1][l2]["*"]...)
+			}
+		}
+		if _, ok := t.subs[l1]["*"]; ok && parts[1] != "*" {
+			l2 := "*"
+			if _, ok := t.subs[l1][l2][parts[2]]; ok {
+				subs = append(subs, t.subs[l1][l2][parts[2]]...)
+			}
+			if _, ok := t.subs[l1][l2]["*"]; ok && parts[2] != "*" {
+				subs = append(subs, t.subs[l1][l2]["*"]...)
+			}
+		}
+	}
+	if _, ok := t.subs["*"]; ok && parts[0] != "*" {
+		l1 := "*"
+		if _, ok := t.subs[l1][parts[1]]; ok {
+			l2 := parts[1]
+			if _, ok := t.subs[l1][l2][parts[2]]; ok {
+				subs = append(subs, t.subs[l1][l2][parts[2]]...)
+			}
+			if _, ok := t.subs[l1][l2]["*"]; ok && parts[2] != "*" {
+				subs = append(subs, t.subs[l1][l2]["*"]...)
+			}
+		}
+		if _, ok := t.subs[l1]["*"]; ok && parts[1] != "*" {
+			l2 := "*"
+			if _, ok := t.subs[l1][l2][parts[2]]; ok {
+				subs = append(subs, t.subs[l1][l2][parts[2]]...)
+			}
+			if _, ok := t.subs[l1][l2]["*"]; ok && parts[2] != "*" {
+				subs = append(subs, t.subs[l1][l2]["*"]...)
+			}
+		}
+	}
+	return
 }
 
 func (t *ticketEventBus) Publish(subject string, data interface{}) error {
+	parts := strings.Split(subject, t.separator)
+	if len(parts) != 3 {
+		return domain.ErrEventKeyInvalid
+	}
+
+	funcs := t.match(subject)
+	for _, f := range funcs {
+		go f(subject, data)
+	}
+
 	return nil
 }
 
