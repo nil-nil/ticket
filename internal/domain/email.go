f286dea54cf6fc3004e9d9f45ae4c374cc9ba231
tom.com <78278478+nil-nil@users.noreply.github.com>
2023-09-18 19:17:54 +0100

    Add email model

---
+++ internal/domain/email.go
@@ -0,0 +1,29 @@
+package domain
+
+import (
+	"context"
+	"net/mail"
+	"time"
+)
+
+type Email struct {
+	ID      uint64
+	Subject string
+	Date    time.Time
+	Message mail.Message
+}
+
+type CreateEmailRepository interface {
+	CreateEmail(ctx context.Context, email Email) (Email, error)
+}
+
+func CreateEmail(ctx context.Context, repo CreateEmailRepository, msg mail.Message) (Email, error) {
+	date, err := msg.Header.Date()
+	if err != nil {
+		date = time.Now()
+	}
+
+	subject := msg.Header.Get("subject")
+
+	return repo.CreateEmail(ctx, Email{Message: msg, Date: date, Subject: subject})
+}
